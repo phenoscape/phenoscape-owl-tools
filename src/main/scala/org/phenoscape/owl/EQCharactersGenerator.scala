8da18e0b76dc6aaa24d825f341193e906e4959a8
Jim Balhoff <balhoff@nescent.org>
2013-11-15 14:03:41 -0600

    Some refactoring.

--- src/main/scala/org/phenoscape/owl/EQCharactersGenerator.scala
+++ src/main/scala/org/phenoscape/owl/EQCharactersGenerator.scala
@@ -13,38 +13,43 @@ import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom
 
 object EQCharactersGenerator {
 
-  val factory = OWLManager.getOWLDataFactory();
-  val manager = OWLManager.createOWLOntologyManager();
-  val partOf = ObjectProperty(Vocab.PART_OF);
-  val involves = ObjectProperty(Vocab.INVOLVES);
-  val eqCharacterToken = Class(Vocab.EQ_CHARACTER_TOKEN);
-  val entityTerm = factory.getOWLAnnotationProperty(IRI.create("http://example.org/entity_term")); //FIXME better ID
-  val qualityTerm = factory.getOWLAnnotationProperty(IRI.create("http://example.org/quality_term")); //FIXME better ID
-  val mayHaveState = factory.getOWLObjectProperty(Vocab.MAY_HAVE_STATE_VALUE);
-  val dcDescription = factory.getOWLAnnotationProperty(DublinCoreVocabulary.DESCRIPTION.getIRI());
+  val factory = OWLManager.getOWLDataFactory
+  val manager = OWLManager.createOWLOntologyManager()
+  val partOf = ObjectProperty(Vocab.PART_OF)
+  val involves = ObjectProperty(Vocab.INVOLVES)
+  val eqCharacterToken = Class(Vocab.EQ_CHARACTER_TOKEN)
+  val entityTerm = factory.getOWLAnnotationProperty(IRI.create("http://example.org/entity_term")) //FIXME better ID
+  val qualityTerm = factory.getOWLAnnotationProperty(IRI.create("http://example.org/quality_term")) //FIXME better ID
+  val anatomicalProjection = Class("http://purl.obolibrary.org/obo/UBERON_0004529")
+  val mayHaveState = factory.getOWLObjectProperty(Vocab.MAY_HAVE_STATE_VALUE)
+  val dcDescription = factory.getOWLAnnotationProperty(DublinCoreVocabulary.DESCRIPTION.getIRI)
 
   def generateEQCharacters(entities: Set[OWLClass], qualities: Set[OWLClass]): Set[OWLAxiom] = {
-    (for (entity <- entities; quality <- qualities) yield composeEntityAndQuality(entity, quality)).flatten;
+    for {
+      entity <- entities
+      quality <- qualities
+      axiom <- composeEntityAndQuality(entity, quality)
+    } yield axiom
   }
 
   def composeEntityAndQuality(entity: OWLClass, quality: OWLClass): Set[OWLAxiom] = {
-    annotateComposedEntityAndQuality(entity, quality).toSet + composeEntityAndQualityInvolves(entity, quality);
+    annotateComposedEntityAndQuality(entity, quality).toSet + composeEntityAndQualityInvolves(entity, quality)
   }
 
   def composeEntityAndQualityInvolves(entity: OWLClass, quality: OWLClass): OWLEquivalentClassesAxiom = {
-    val composition = Class(compositionIRI(entity, quality));
-    composition EquivalentTo ((involves some entity) and (involves some quality) and eqCharacterToken);
+    val composition = Class(compositionIRI(entity, quality))
+    composition EquivalentTo ((involves some entity) and (involves some quality) and eqCharacterToken)
   }
 
   def annotateComposedEntityAndQuality(entity: OWLClass, quality: OWLClass): Set[OWLAnnotationAssertionAxiom] = {
-    val subject = compositionIRI(entity, quality);
-    val entityAxiom = factory.getOWLAnnotationAssertionAxiom(entityTerm, subject, entity.getIRI());
-    val qualityAxiom = factory.getOWLAnnotationAssertionAxiom(qualityTerm, subject, quality.getIRI());
-    Set(entityAxiom, qualityAxiom);
+    val subject = compositionIRI(entity, quality)
+    val entityAxiom = subject Annotation (entityTerm, entity.getIRI)
+    val qualityAxiom = subject Annotation (qualityTerm, quality.getIRI)
+    Set(entityAxiom, qualityAxiom)
   }
 
   def compositionIRI(entity: OWLClass, quality: OWLClass): IRI = {
-    IRI.create("http://example.org/involves?entity=%s&quality=%s".format(entity.getIRI(), quality.getIRI()));
+    IRI.create(s"http://example.org/involves?entity=${entity.getIRI}&quality=${quality.getIRI}")
   }
 
 }
