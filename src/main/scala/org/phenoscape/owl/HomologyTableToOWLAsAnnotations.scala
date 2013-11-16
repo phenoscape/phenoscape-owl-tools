05b01c2688b2d4769bd5c617e83c09a3f3c9b0b7
Jim Balhoff <balhoff@nescent.org>
2013-11-15 22:48:53 -0600

    Refactoring for style.

--- src/main/scala/org/phenoscape/owl/HomologyTableToOWLAsAnnotations.scala
+++ src/main/scala/org/phenoscape/owl/HomologyTableToOWLAsAnnotations.scala
@@ -18,41 +18,41 @@ import org.semanticweb.owlapi.vocab.DublinCoreVocabulary
 
 object HomologyTableToOWLAsAnnotations extends OWLTask {
 
-  val manager = this.getOWLOntologyManager;
-  val homologousTo = ObjectProperty(Vocab.HOMOLOGOUS_TO);
-  val hasEvidence = ObjectProperty(Vocab.EVIDENCE);
-  val source = factory.getOWLAnnotationProperty(DublinCoreVocabulary.SOURCE.getIRI());
-  val description = factory.getOWLAnnotationProperty(DublinCoreVocabulary.DESCRIPTION.getIRI());
-  val aboutStructure = ObjectProperty("http://example.org/about_structure");
-  val homologyAnnotation = Class("http://example.org/HomologyAnnotation");
-  val negativeHomologyAnnotation = Class("http://example.org/NegativeHomologyAnnotation");
+  val manager = this.getOWLOntologyManager
+  val homologousTo = ObjectProperty(Vocab.HOMOLOGOUS_TO)
+  val hasEvidence = ObjectProperty(Vocab.EVIDENCE)
+  val source = factory.getOWLAnnotationProperty(DublinCoreVocabulary.SOURCE.getIRI)
+  val description = factory.getOWLAnnotationProperty(DublinCoreVocabulary.DESCRIPTION.getIRI)
+  val aboutStructure = ObjectProperty("http://example.org/about_structure")
+  val homologyAnnotation = Class("http://example.org/HomologyAnnotation")
+  val negativeHomologyAnnotation = Class("http://example.org/NegativeHomologyAnnotation")
 
   def main(args: Array[String]): Unit = {
-    val input = Source.fromFile(args(0), "utf-8");
-    val output = convertFile(input);
-    manager.saveOntology(output, IRI.create(new File(args(1))));
+    val input = Source.fromFile(args(0), "utf-8")
+    val output = convertFile(input)
+    manager.saveOntology(output, IRI.create(new File(args(1))))
   }
 
   def convertFile(file: Source): OWLOntology = {
-    val axioms = file.getLines().drop(1).map(processEntry(_)).flatten.toSet;
-    val ontology = manager.createOntology(IRI.create("http://purl.obolibrary.org/obo/uberon/homology_annotations.owl"));
-    manager.applyChange(new AddOntologyAnnotation(ontology, factory.getOWLAnnotation(description, factory.getOWLLiteral("Homology Assertions"))));
-    manager.applyChange(new AddImport(ontology, factory.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/uberon/ext.owl"))));
-    manager.applyChange(new AddImport(ontology, factory.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/eco.owl"))));
-    manager.addAxioms(ontology, axioms);
-    return ontology;
+    val axioms = (file.getLines.drop(1) flatMap processEntry).toSet
+    val ontology = manager.createOntology(IRI.create("http://purl.obolibrary.org/obo/uberon/homology_annotations.owl"))
+    manager.applyChange(new AddOntologyAnnotation(ontology, factory.getOWLAnnotation(description, factory.getOWLLiteral("Homology Assertions"))))
+    manager.applyChange(new AddImport(ontology, factory.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/uberon/ext.owl"))))
+    manager.applyChange(new AddImport(ontology, factory.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/eco.owl"))))
+    manager.addAxioms(ontology, axioms)
+    ontology
   }
 
   def processEntry(line: String): Set[OWLAxiom] = {
-    val items = line.split("\t", -1);
-    val annotation = Individual("http://example.org/" + UUID.randomUUID().toString());
-    val structure1 = Individual(IRI.create(items(1).trim()));
-    val structure2 = Individual(IRI.create(items(6).trim()));
-    val evidenceCode = Class(OBOUtil.iriForTermID(items(10).trim()));
-    val evidence = Individual("http://example.org/" + UUID.randomUUID().toString());
-    val pub = factory.getOWLLiteral(items(11).trim());
+    val items = line.split("\t", -1)
+    val annotation = Individual("http://example.org/" + UUID.randomUUID().toString)
+    val structure1 = Individual(IRI.create(items(1).trim))
+    val structure2 = Individual(IRI.create(items(6).trim))
+    val evidenceCode = Class(OBOUtil.iriForTermID(items(10).trim))
+    val evidence = Individual("http://example.org/" + UUID.randomUUID().toString)
+    val pub = factory.getOWLLiteral(items(11).trim)
     Set(
-      if (items(4).trim() == "hom to") {
+      if (items(4).trim == "hom to") {
         annotation Type homologyAnnotation
       } else {
         annotation Type negativeHomologyAnnotation
@@ -61,8 +61,7 @@ object HomologyTableToOWLAsAnnotations extends OWLTask {
       annotation Fact (aboutStructure, structure2),
       annotation Fact (hasEvidence, evidence),
       evidence Type evidenceCode,
-      evidence Annotation (source, pub));
-
+      evidence Annotation (source, pub))
   }
 
 }
--- src/main/scala/org/phenoscape/owl/NegationHierarchyAsserter.scala
+++ src/main/scala/org/phenoscape/owl/NegationHierarchyAsserter.scala
@@ -2,10 +2,7 @@ package org.phenoscape.owl
 
 import java.io.File
 import scala.collection.JavaConversions._
-import scala.collection.JavaConverters._
 import scala.collection.Set
-import scala.collection.Map
-import scala.collection.mutable
 import org.semanticweb.owlapi.apibinding.OWLManager
 import org.semanticweb.owlapi.model.AxiomType
 import org.semanticweb.owlapi.model.IRI
@@ -13,41 +10,44 @@ import org.semanticweb.owlapi.model.OWLClass
 import org.semanticweb.owlapi.model.OWLOntology
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom
 import org.semanticweb.owlapi.model.OWLAxiom
+import org.phenoscape.scowl.OWL._
 
 object NegationHierarchyAsserter extends OWLTask {
 
-  val negates = factory.getOWLAnnotationProperty(Vocab.NEGATES);
+  val negates = factory.getOWLAnnotationProperty(Vocab.NEGATES)
 
   def main(args: Array[String]): Unit = {
-    val manager = this.getOWLOntologyManager();
-    val ontology = manager.loadOntologyFromOntologyDocument(new File(args(0)));
-    assertNegationHierarchy(ontology);
+    val manager = this.getOWLOntologyManager()
+    val ontology = manager.loadOntologyFromOntologyDocument(new File(args(0)))
+    assertNegationHierarchy(ontology)
     if (args.size > 1) {
-      manager.saveOntology(ontology, IRI.create(new File(args(1))));
+      manager.saveOntology(ontology, IRI.create(new File(args(1))))
     } else {
-      manager.saveOntology(ontology);
+      manager.saveOntology(ontology)
     }
   }
 
   def assertNegationHierarchy(ontologies: OWLOntology*): Set[OWLAxiom] = {
-    val allClasses = ontologies.map(_.getClassesInSignature(true)).flatten;
-    val negatesIndex: mutable.Map[IRI, mutable.Set[IRI]] = mutable.Map();
-    val negatedByIndex: mutable.Map[IRI, mutable.Set[IRI]] = mutable.Map();
-    ontologies.foreach(ont => {
-      val annotations = ont.getAxioms(AxiomType.ANNOTATION_ASSERTION, false).filter(_.getProperty() == negates);
-      annotations.foreach(annot => {
-        negatesIndex.getOrElseUpdate(annot.getSubject().asInstanceOf[IRI], mutable.Set()).add(annot.getValue().asInstanceOf[IRI]);
-        negatedByIndex.getOrElseUpdate(annot.getValue().asInstanceOf[IRI], mutable.Set()).add(annot.getSubject().asInstanceOf[IRI]);
-      });
-    });
-    val axioms = allClasses.map(createSubclassOfAxioms(_, negatesIndex, negatedByIndex, ontologies.toSet));
-    axioms.toSet.flatten;
+    val allClasses = ontologies flatMap (_.getClassesInSignature(true))
+    val negatesPairs = for {
+      ont <- ontologies
+      annotationAxiom <- ont.getAxioms(AxiomType.ANNOTATION_ASSERTION, false)
+      if annotationAxiom.getProperty() == negates
+    } yield (annotationAxiom.getSubject().asInstanceOf[IRI], annotationAxiom.getValue().asInstanceOf[IRI])
+    val emptyIndex = Map[IRI, Set[IRI]]().withDefaultValue(Set())
+    val negatesIndex = negatesPairs.foldLeft(emptyIndex)((in, item) => in.updated(item._1, (in(item._1) + item._2)))
+    val negatedByIndex = negatesPairs.foldLeft(emptyIndex)((in, item) => in.updated(item._2, (in(item._2) + item._1)))
+    val axioms = allClasses.flatMap(createSubclassOfAxioms(_, negatesIndex, negatedByIndex, ontologies.toSet))
+    axioms.toSet
   }
 
   def createSubclassOfAxioms(ontClass: OWLClass, negatesIndex: Map[IRI, Set[IRI]], negatedByIndex: Map[IRI, Set[IRI]], ontologies: Set[OWLOntology]): Iterable[OWLSubClassOfAxiom] = {
-    val factory = OWLManager.getOWLDataFactory();
-    val superClasses = negatesIndex.get(ontClass.getIRI()).toSet.flatten.map(factory.getOWLClass(_)).map(_.getSubClasses(ontologies).filter(!_.isAnonymous()).map(_.asOWLClass())).flatten.map(c => negatedByIndex.get(c.getIRI()).toSet.flatten.map(factory.getOWLClass(_))).flatten;
-    superClasses.map(factory.getOWLSubClassOfAxiom(ontClass, _));
+    for {
+      negatedClassIRI <- negatesIndex.getOrElse(ontClass.getIRI, Set())
+      subClassOfNegatedClass <- Class(negatedClassIRI).getSubClasses(ontologies)
+      if !subClassOfNegatedClass.isAnonymous()
+      superClassOfOntClassIRI <- negatedByIndex.getOrElse(subClassOfNegatedClass.asOWLClass.getIRI, Set())
+    } yield ontClass SubClassOf Class(superClassOfOntClassIRI)
   }
 
 }
