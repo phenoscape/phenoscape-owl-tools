8e18f1f0154771dea5408d59343f9f18f93f1efa
Jim Balhoff <balhoff@nescent.org>
2013-12-03 15:39:03 -0600

    Bugfix for wrong column index in ZFIN expression converter.

--- src/main/scala/org/phenoscape/owl/mod/zfin/ZFINExpressionToOWL.scala
+++ src/main/scala/org/phenoscape/owl/mod/zfin/ZFINExpressionToOWL.scala
@@ -20,56 +20,55 @@ import org.semanticweb.owlapi.apibinding.OWLManager
 
 object ZFINExpressionToOWL extends OWLTask {
 
-  val geneExpression = Class(Vocab.GENE_EXPRESSION);
-  val zebrafish = Individual(Vocab.ZEBRAFISH);
-  val manager = OWLManager.createOWLOntologyManager();
+  val geneExpression = Class(Vocab.GENE_EXPRESSION)
+  val zebrafish = Individual(Vocab.ZEBRAFISH)
+  val manager = OWLManager.createOWLOntologyManager()
 
   def main(args: Array[String]): Unit = {
-    val file = Source.fromFile(args(0), "ISO-8859-1");
-    val ontology = convert(file);
-    file.close();
-    manager.saveOntology(ontology, IRI.create(new File(args(1))));
+    val file = Source.fromFile(args(0), "ISO-8859-1")
+    val ontology = convert(file)
+    file.close()
+    manager.saveOntology(ontology, IRI.create(new File(args(1))))
   }
 
   def convert(expressionData: Source): OWLOntology = {
-    val ontology = manager.createOntology(IRI.create("http://purl.obolibrary.org/obo/phenoscape/zfin_gene_expression.owl"));
-    manager.addAxioms(ontology, expressionData.getLines.map(translate(_)).flatten.toSet[OWLAxiom]);
-    return ontology;
+    val axioms = expressionData.getLines.map(translate(_)).flatten.toSet[OWLAxiom]
+    manager.createOntology(axioms, IRI.create("http://purl.obolibrary.org/obo/phenoscape/zfin_gene_expression.owl"))
   }
 
   def translate(expressionLine: String): Set[OWLAxiom] = {
-    val items = expressionLine.split("\t", -1);
-    val axioms = mutable.Set[OWLAxiom]();
+    val items = expressionLine.split("\t", -1)
+    val axioms = mutable.Set[OWLAxiom]()
     if (items(0).startsWith("ZDB-EFG")) {
-      return axioms;
+      return axioms
     } else {
-      val expression = nextIndividual();
-      axioms.add(factory.getOWLDeclarationAxiom(expression));
-      axioms.add(expression Type geneExpression);
-      val structure = nextIndividual();
-      axioms.add(factory.getOWLDeclarationAxiom(structure));
-      axioms.add(expression Fact (OCCURS_IN, structure));
-      val superStructureID = StringUtils.stripToNull(items(3));
-      val subStructureID = StringUtils.stripToNull(items(5));
+      val expression = nextIndividual()
+      axioms.add(factory.getOWLDeclarationAxiom(expression))
+      axioms.add(expression Type geneExpression)
+      val structure = nextIndividual()
+      axioms.add(factory.getOWLDeclarationAxiom(structure))
+      axioms.add(expression Fact (OCCURS_IN, structure))
+      val superStructureID = StringUtils.stripToNull(items(3))
+      val subStructureID = StringUtils.stripToNull(items(5))
       if (subStructureID == null) {
-        val structureType = Class(OBOUtil.iriForTermID(superStructureID));
-        axioms.add(structure Type structureType);
+        val structureType = Class(OBOUtil.iriForTermID(superStructureID))
+        axioms.add(structure Type structureType)
       } else {
-        val superStructure = Class(OBOUtil.iriForTermID(superStructureID));
-        val subStructure = Class(OBOUtil.iriForTermID(subStructureID));
-        val structureType = nextClass();
-        axioms.add(structureType SubClassOf (subStructure and (part_of some superStructure)));
-        axioms.add(structure Type structureType);
+        val superStructure = Class(OBOUtil.iriForTermID(superStructureID))
+        val subStructure = Class(OBOUtil.iriForTermID(subStructureID))
+        val structureType = nextClass()
+        axioms.add(structureType SubClassOf (subStructure and (part_of some superStructure)))
+        axioms.add(structure Type structureType)
       }
-      val geneIRI = IRI.create("http://zfin.org/" + StringUtils.stripToNull(items(0)));
-      val gene = Individual(geneIRI);
-      axioms.add(factory.getOWLDeclarationAxiom(gene));
-      axioms.add(expression Fact (ASSOCIATED_WITH_GENE, gene));
-      axioms.add(expression Fact (ASSOCIATED_WITH_TAXON, zebrafish));
-      val publicationID = StringUtils.stripToNull(items(25))
+      val geneIRI = OBOUtil.zfinIRI(StringUtils.stripToNull(items(0)))
+      val gene = Individual(geneIRI)
+      axioms.add(factory.getOWLDeclarationAxiom(gene))
+      axioms.add(expression Fact (ASSOCIATED_WITH_GENE, gene))
+      axioms.add(expression Fact (ASSOCIATED_WITH_TAXON, zebrafish))
+      val publicationID = StringUtils.stripToNull(items(10))
       val publication = Individual(OBOUtil.zfinIRI(publicationID))
       axioms.add(expression Fact (dcSource, publication))
-      return axioms;
+      return axioms
     }
   }
 
