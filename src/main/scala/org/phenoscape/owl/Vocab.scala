41831eeec58d39fe49c3ab1b762962d65a368ee7
Jim Balhoff <balhoff@nescent.org>
2013-12-06 15:28:00 -0600

    Added term to vocab.

--- src/main/scala/org/phenoscape/owl/Vocab.scala
+++ src/main/scala/org/phenoscape/owl/Vocab.scala
@@ -70,5 +70,6 @@ object Vocab {
   val EQ_CHARACTER_TOKEN = IRI.create("http://purl.org/phenoscape/vocab.owl#EQCharacterToken")
   val HAS_NUMBER_OF = IRI.create("http://purl.obolibrary.org/obo/PATO_0001555")
   val dcSource = ObjectProperty(IRI.create("http://purl.org/dc/terms/source"))
+  val connected_to = ObjectProperty(IRI.create("http://purl.obolibrary.org/obo/core#connected_to"))
 
 }
