a4f8475505b344c6bd6901932fe12601875b1c44
Jim Balhoff <balhoff@nescent.org>
2014-05-19 10:18:39 -0500

    Testing methods for permuting class expressions, for generalization.

--- src/main/scala/org/phenoscape/owl/util/ExpressionUtil.scala
+++ src/main/scala/org/phenoscape/owl/util/ExpressionUtil.scala
@@ -3,7 +3,6 @@ package org.phenoscape.owl.util
 import java.io.StringWriter
 import java.util.ArrayList
 import scala.collection.JavaConversions._
-import scala.collection.Set
 import scala.collection.mutable
 import org.phenoscape.owl.OWLTask
 import org.phenoscape.owl.OWLTask
@@ -24,12 +23,16 @@ import org.semanticweb.owlapi.util.AnnotationValueShortFormProvider
 import uk.ac.manchester.cs.owl.owlapi.mansyntaxrenderer.ManchesterOWLSyntaxObjectRenderer
 import uk.ac.manchester.cs.owl.owlapi.mansyntaxrenderer.ManchesterOWLSyntaxOWLObjectRendererImpl
 import org.semanticweb.owlapi.model.OWLObject
+import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom
+import org.semanticweb.owlapi.reasoner.OWLReasoner
+import org.phenoscape.scowl.OWL._
+import org.semanticweb.owlapi.model.OWLClass
 
 object ExpressionUtil {
 
   val factory = OWLManager.getOWLDataFactory
 
-  def instantiateClassAssertion(individual: OWLIndividual, aClass: OWLClassExpression, context: OWLTask): Set[OWLAxiom] = {
+  def instantiateClassAssertion(individual: OWLIndividual, aClass: OWLClassExpression, context: OWLTask): scala.collection.Set[OWLAxiom] = {
     val axioms = mutable.Set[OWLAxiom]()
     if (aClass.isInstanceOf[OWLQuantifiedObjectRestriction]) { // either someValuesFrom or allValuesFrom
       val restriction = aClass.asInstanceOf[OWLQuantifiedObjectRestriction]
@@ -67,6 +70,32 @@ object ExpressionUtil {
     val renderer = new ManchesterOWLSyntaxOWLObjectRendererImpl()
     renderer.setShortFormProvider(shortFormProvider)
     entity => renderer.render(entity).replaceAll("\n", " ").replaceAll("\\s+", " ")
+  }
+
+  def permute(expression: OWLClassExpression)(implicit reasoner: OWLReasoner): Set[OWLClassExpression] = expression match {
+    case namedClass: OWLClass => reasoner.getSuperClasses(namedClass, true).getFlattened.toSet + namedClass
+    case someValuesFrom: OWLObjectSomeValuesFrom => permute(someValuesFrom)
+    case intersection: OWLObjectIntersectionOf => permute(intersection)
+    case _ => Set(expression)
+  }
+
+  def permute(expression: OWLObjectSomeValuesFrom)(implicit reasoner: OWLReasoner): Set[OWLObjectSomeValuesFrom] = {
+    val property = expression.getProperty
+    reasoner.getSuperClasses(expression.getFiller, true).getFlattened.toSet[OWLClass].map(property some _) + expression
+  }
+
+  def permute(expression: OWLObjectIntersectionOf)(implicit reasoner: OWLReasoner): Set[OWLObjectIntersectionOf] = {
+    val permutedOperands = expression.getOperands.toSet[OWLClassExpression].map(permute)
+    val combinations = allCombinations(permutedOperands)
+    combinations.map(factory.getOWLObjectIntersectionOf(_)) + expression
+  }
+
+  def allCombinations[T](sets: Set[Set[T]]): Set[Set[T]] = {
+    def combine[T](combinations: Set[Set[T]], itemsToCombine: Set[T]): Set[Set[T]] = for {
+      combination <- combinations
+      item <- itemsToCombine
+    } yield combination + item
+    sets.foldLeft(Set[Set[T]]())(combine)
   }
 
 }
