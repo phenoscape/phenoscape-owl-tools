470f2cff98e2898f001a1d17ece2adc4d3d7a973
Jim Balhoff <balhoff@nescent.org>
2015-03-17 09:41:07 -0400

    Modified turtle loader to handle multiple files.

--- src/main/scala/org/phenoscape/owl/build/LoadTriples.scala
+++ src/main/scala/org/phenoscape/owl/build/LoadTriples.scala
@@ -3,19 +3,19 @@ package org.phenoscape.owl.build
 import java.io.File
 import java.io.FileReader
 import java.util.Properties
-
 import org.openrdf.model.impl.URIImpl
 import org.openrdf.rio.RDFFormat
-
 import com.bigdata.journal.Options
 import com.bigdata.rdf.sail.BigdataSail
 import com.bigdata.rdf.sail.BigdataSailRepository
+import org.apache.commons.io.FileUtils
+import scala.collection.JavaConversions._
 
 object LoadTriples extends App {
 
   val bigdataPropertiesFiles = new File(args(0))
   val bigdataJournalFile = new File(args(1))
-  val inputFile = new File(args(2))
+  val inputFolder = new File(args(2))
   val graphURI = new URIImpl(args(3))
   val bigdataProperties = new Properties()
   bigdataProperties.load(new FileReader(bigdataPropertiesFiles))
@@ -25,9 +25,13 @@ object LoadTriples extends App {
   repository.initialize()
   val bigdata = repository.getUnisolatedConnection()
   val baseURI = ""
-  bigdata.begin()
-  bigdata.add(inputFile, baseURI, RDFFormat.TURTLE, graphURI)
-  bigdata.commit()
+  for (triplesFile <- FileUtils.listFiles(inputFolder, Array("ttl"), true)) {
+    println(s"Loading $triplesFile")
+    bigdata.begin()
+    bigdata.add(triplesFile, baseURI, RDFFormat.TURTLE, graphURI)
+    bigdata.commit()
+    println(s"Done loading $triplesFile")
+  }
   bigdata.close()
 
 }
