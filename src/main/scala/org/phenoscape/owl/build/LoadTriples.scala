c2bdbeaca08a4e2adf8c3a27f7fc5d7c92107cba
Jim Balhoff <balhoff@nescent.org>
2015-03-17 14:43:28 -0400

    Switched to alternative data load API.

--- src/main/scala/org/phenoscape/owl/build/LoadTriples.scala
+++ src/main/scala/org/phenoscape/owl/build/LoadTriples.scala
@@ -11,31 +11,36 @@ import com.bigdata.rdf.sail.BigdataSailRepository
 import org.apache.commons.io.FileUtils
 import scala.collection.JavaConversions._
 import java.util.Date
+import com.bigdata.rdf.store.DataLoader
 
 object LoadTriples extends App {
 
   val bigdataPropertiesFiles = new File(args(0))
   val bigdataJournalFile = new File(args(1))
   val inputFolder = new File(args(2))
-  val graphURI = new URIImpl(args(3))
+  val graphURI = args(3)
   val bigdataProperties = new Properties()
   bigdataProperties.load(new FileReader(bigdataPropertiesFiles))
   bigdataProperties.setProperty(Options.FILE, bigdataJournalFile.getAbsolutePath)
   val sail = new BigdataSail(bigdataProperties)
-  val repository = new BigdataSailRepository(sail)
-  repository.initialize()
-  val bigdata = repository.getUnisolatedConnection()
+  //val repository = new BigdataSailRepository(sail)
+  val tripleStore = sail.getDatabase
+  //repository.initialize()
+  //val bigdata = repository.getUnisolatedConnection()
+  val loader = new DataLoader(tripleStore)
   val baseURI = ""
   for (triplesFile <- FileUtils.listFiles(inputFolder, Array("ttl"), true)) {
     val start = new Date()
     println(s"Loading $triplesFile")
-    bigdata.begin()
-    bigdata.add(triplesFile, baseURI, RDFFormat.TURTLE, graphURI)
-    bigdata.commit()
+    val stats = loader.loadFiles(triplesFile, baseURI, RDFFormat.TURTLE, graphURI, null)
+    println(stats)
+    //bigdata.begin()
+    //bigdata.add(triplesFile, baseURI, RDFFormat.TURTLE, graphURI)
+    //bigdata.commit()
     val end = new Date()
     val seconds = (end.getTime - start.getTime).toDouble / 1000
     println(s"Done loading $triplesFile; $seconds seconds")
   }
-  bigdata.close()
+  //bigdata.close()
 
 }
