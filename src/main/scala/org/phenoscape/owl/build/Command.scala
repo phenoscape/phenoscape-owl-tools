a0ce7e8939330e3338e07775f7ea1c46d07b6b27
Jim Balhoff <jim@balhoff.org>
2015-11-20 09:08:21 -0500

    Corrected name of RDF bulk loader main class.

--- src/main/scala/org/phenoscape/owl/build/Command.scala
+++ src/main/scala/org/phenoscape/owl/build/Command.scala
@@ -1,12 +1,14 @@
 package org.phenoscape.owl.build
 
+import com.bigdata.rdf.store.DataLoader
+
 object Command extends App {
 
   val commandArgs = args.drop(1)
   args(0) match {
     case "build-kb"             => PhenoscapeKB.main(commandArgs)
     case "pairwise-sim"         => RunPairwiseOWLSim.main(commandArgs)
-    case "load-triples"         => LoadTriples.main(commandArgs)
+    case "load-triples"         => DataLoader.main(commandArgs)
     case "output-ics"           => ComputeICs.main(commandArgs)
     //case "output-match-scores"  => //requires sparql query to DB and output tab-delimited; just run sparql query
     case "output-profile-sizes" => PrintTaxonAndGeneProfileSizes.main(commandArgs)
