87c1212872e8911b85545cc96d14403c5c1a5e54
Jim Balhoff <jim@balhoff.org>
2016-01-13 10:18:49 -0500

    Fixed NegationHierarchyAsserter to handle equivalent class axioms.

--- src/test/scala/org/phenoscape/owl/NegationHierarchyAsserterTest.scala
+++ src/test/scala/org/phenoscape/owl/NegationHierarchyAsserterTest.scala
@@ -5,23 +5,30 @@ import scala.collection.JavaConversions._
 import org.junit.Test
 import org.semanticweb.owlapi.apibinding.OWLManager
 
+import org.phenoscape.scowl.OWL._
+
 import junit.framework.Assert
 
 class NegationHierarchyAsserterTest {
 
+  val base = "http://owl.phenoscape.org/NegationHierarchyAsserterTest"
+
   @Test
   def testHierarchy() {
     val manager = OWLManager.createOWLOntologyManager()
-    val input = getClass().getClassLoader().getResourceAsStream("NegationHierarchyAsserterTest.owl")
-    val expectation = getClass().getClassLoader().getResourceAsStream("NegationHierarchyAsserterTestExpectation.owl")
+    val input = getClass().getClassLoader().getResourceAsStream("NegationHierarchyAsserterTest.ofn")
     val ontology = manager.loadOntologyFromOntologyDocument(input)
-    val expectedOntology = manager.loadOntologyFromOntologyDocument(expectation)
     input.close()
-    expectation.close()
-    Assert.assertTrue(ontology.getAxiomCount() < expectedOntology.getAxiomCount())
+    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#B") SubClassOf Class(s"$base#A")))
+    Assert.assertFalse(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB")))
+    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#C") SubClassOf Class(s"$base#B")))
+    Assert.assertFalse(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC")))
+    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#B") EquivalentTo Class(s"$base#D")))
+    Assert.assertFalse(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD")))
     manager.addAxioms(ontology, NegationHierarchyAsserter.assertNegationHierarchy(ontology.getAxioms.toSet))
-    Assert.assertTrue(ontology.getAxiomCount() == expectedOntology.getAxiomCount())
-    Assert.assertEquals(ontology.getAxioms(), expectedOntology.getAxioms())
+    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB")))
+    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC")))
+    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD")))
   }
 
 }
