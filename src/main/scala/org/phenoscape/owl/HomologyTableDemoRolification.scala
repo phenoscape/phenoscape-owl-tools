a66e1adec3da9e3bfe9a0c02c5708dd63b9b95c2
Jim Balhoff <jim@balhoff.org>
2017-12-07 13:03:33 -0500

    Added alternative homology model demos.

---
+++ src/main/scala/org/phenoscape/owl/HomologyTableDemoRolification.scala
@@ -0,0 +1,94 @@
+package org.phenoscape.owl
+
+import java.io.File
+import java.util.UUID
+
+import scala.collection.JavaConversions._
+import scala.io.Source
+
+import org.phenoscape.kb.ingest.util.OBOUtil
+import org.phenoscape.scowl._
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.AddImport
+import org.semanticweb.owlapi.model.AddOntologyAnnotation
+import org.semanticweb.owlapi.model.IRI
+import org.semanticweb.owlapi.model.OWLAxiom
+import org.semanticweb.owlapi.model.OWLOntology
+import org.semanticweb.owlapi.vocab.DublinCoreVocabulary
+
+import Vocab._
+import org.phenoscape.kb.ingest.util.PostCompositionParser
+import org.semanticweb.owlapi.model.OWLClassExpression
+import org.semanticweb.owlapi.model.OWLNamedIndividual
+import org.semanticweb.owlapi.model.OWLIndividual
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.lang3.StringUtils
+
+object HomologyTableDemoRolification extends App {
+
+  val factory = OWLManager.getOWLDataFactory
+  val manager = OWLManager.createOWLOntologyManager
+  val source = AnnotationProperty(DublinCoreVocabulary.SOURCE.getIRI)
+  val description = AnnotationProperty(DublinCoreVocabulary.DESCRIPTION.getIRI)
+  val InHistoricalHomologyRelationshipWith = ObjectProperty("http://purl.obolibrary.org/obo/RO_HOM0000007")
+  val SeriallyHomologousTo = ObjectProperty("http://purl.obolibrary.org/obo/RO_HOM0000027")
+
+  val input = Source.fromFile(args(0), "utf-8")
+
+  def convertFile(file: Source): OWLOntology = {
+    val axioms = (file.getLines.drop(1).flatMap(processEntry)).toSet
+    val ontology = manager.createOntology(axioms, IRI.create("http://purl.org/phenoscape/demo/phenoscape_homology.owl"))
+    manager.applyChange(new AddOntologyAnnotation(ontology, factory.getOWLAnnotation(description, factory.getOWLLiteral("Homology Assertions"))))
+    //manager.applyChange(new AddImport(ontology, factory.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/uberon/ext.owl"))))
+    //manager.applyChange(new AddImport(ontology, factory.getOWLImportsDeclaration(IRI.create("http://purl.obolibrary.org/obo/eco.owl"))))
+    ontology
+  }
+
+  def processEntry(line: String): Set[OWLAxiom] = {
+    val items = line.split("\t", -1)
+    val uniqueID = DigestUtils.sha1Hex(line)
+    val uniquePrefix = s"http://purl.phenoscape.org/homology/annotation/$uniqueID"
+    val relation = items(4).trim
+    val (property, negated) = relation match {
+      case "hom to"         => (InHistoricalHomologyRelationshipWith, false)
+      case "ser hom to"     => (SeriallyHomologousTo, false)
+      case "not hom to"     => (InHistoricalHomologyRelationshipWith, true)
+      case "not ser hom to" => (SeriallyHomologousTo, true)
+    }
+    val structure1Text = items(1).trim
+    val structure1 = if (structure1Text.contains("^")) PostCompositionParser.parseExpression(structure1Text).get
+    else Class(IRI.create(structure1Text))
+    val taxon1 = Class(IRI.create(items(3).trim))
+    val role1 = ObjectProperty(s"$uniquePrefix#role1")
+    val structure2Text = items(6).trim
+    val structure2 = if (structure2Text.contains("^")) PostCompositionParser.parseExpression(structure2Text).get
+    else Class(IRI.create(structure2Text))
+    val taxon2 = Class(IRI.create(items(8).trim))
+    val role2 = ObjectProperty(s"$uniquePrefix#role2")
+    var axioms = Set.empty[OWLAxiom]
+    Option(StringUtils.stripToNull(items(12))) match {
+      case Some(evidenceID) =>
+        val evidenceCode = Class(OBOUtil.iriForTermID(evidenceID))
+        val evidence = Individual(s"$uniquePrefix#evidence")
+        val pub = factory.getOWLLiteral(items(13).trim)
+        if (!negated) {
+          axioms += ((structure1 and (in_taxon some taxon1)) SubClassOf (role1.Self)) Annotation (axiom_has_evidence, evidence)
+          axioms += ((structure2 and (in_taxon some taxon2)) SubClassOf (role2.Self)) Annotation (axiom_has_evidence, evidence)
+          axioms += (property SubPropertyChain (role1 o factory.getOWLTopObjectProperty o role2)) Annotation (axiom_has_evidence, evidence)
+        }
+        axioms += evidence Type evidenceCode
+        axioms += evidence Annotation (source, pub)
+      case None =>
+        if (!negated) {
+          axioms += ((structure1 and (in_taxon some taxon1)) SubClassOf (role1.Self))
+          axioms += ((structure2 and (in_taxon some taxon2)) SubClassOf (role2.Self))
+          axioms += (property SubPropertyChain (role1 o factory.getOWLTopObjectProperty o role2))
+        }
+    }
+    axioms
+  }
+
+  val output = convertFile(input)
+  manager.saveOntology(output, IRI.create(new File(args(1))))
+
+}
---
+++ src/main/scala/org/phenoscape/owl/HomologyTableToOWLVAHM.scala
@@ -0,0 +1,91 @@
+package org.phenoscape.owl
+
+import java.io.File
+
+import scala.collection.JavaConversions._
+import scala.io.Source
+
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.lang3.StringUtils
+import org.phenoscape.kb.ingest.util.OBOUtil
+import org.phenoscape.kb.ingest.util.PostCompositionParser
+import org.phenoscape.scowl._
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.AddOntologyAnnotation
+import org.semanticweb.owlapi.model.IRI
+import org.semanticweb.owlapi.model.OWLAxiom
+import org.semanticweb.owlapi.model.OWLOntology
+import org.semanticweb.owlapi.vocab.DublinCoreVocabulary
+
+import Vocab._
+
+object HomologyTableToOWLVAHM extends App {
+
+  val factory = OWLManager.getOWLDataFactory
+  val manager = OWLManager.createOWLOntologyManager
+  val source = AnnotationProperty(DublinCoreVocabulary.SOURCE.getIRI)
+  val description = AnnotationProperty(DublinCoreVocabulary.DESCRIPTION.getIRI)
+  val InHistoricalHomologyRelationshipWith = ObjectProperty("http://purl.obolibrary.org/obo/RO_HOM0000007")
+  val SeriallyHomologousTo = ObjectProperty("http://purl.obolibrary.org/obo/RO_HOM0000027")
+  val HistoricalHomologyMemberof = ObjectProperty("http://example.org/historical_homology_member_of")
+  val HasHistoricalHomologyMember = ObjectProperty("http://example.org/has_historical_homology_member")
+  val SerialHomologyMemberOf = ObjectProperty("http://example.org/serial_homology_member_of")
+  val HasSerialHomologyMember = ObjectProperty("http://example.org/has_serial_homology_member")
+
+  val input = Source.fromFile(args(0), "utf-8")
+
+  def convertFile(file: Source): OWLOntology = {
+    val axioms = (file.getLines.drop(1).flatMap(processEntry)).toSet
+    val ontology = manager.createOntology(axioms, IRI.create("http://purl.org/phenoscape/demo/phenoscape_homology.owl"))
+    manager.applyChange(new AddOntologyAnnotation(ontology, factory.getOWLAnnotation(description, factory.getOWLLiteral("Homology Assertions using the VAHM model"))))
+    manager.addAxiom(ontology, HistoricalHomologyMemberof InverseOf HasHistoricalHomologyMember)
+    manager.addAxiom(ontology, SerialHomologyMemberOf InverseOf HasSerialHomologyMember)
+    manager.addAxiom(ontology, InHistoricalHomologyRelationshipWith SubPropertyChain (HistoricalHomologyMemberof o HasHistoricalHomologyMember))
+    manager.addAxiom(ontology, SeriallyHomologousTo SubPropertyChain (SerialHomologyMemberOf o HasSerialHomologyMember))
+    ontology
+  }
+
+  def processEntry(line: String): Set[OWLAxiom] = {
+    val items = line.split("\t", -1)
+    val uniqueID = DigestUtils.sha1Hex(line)
+    val uniquePrefix = s"http://purl.phenoscape.org/homology/annotation/$uniqueID"
+    val relation = items(4).trim
+    val (upProperty, downProperty, negated) = relation match {
+      case "hom to"         => (HistoricalHomologyMemberof, HasHistoricalHomologyMember, false)
+      case "ser hom to"     => (SerialHomologyMemberOf, HasSerialHomologyMember, false)
+      case "not hom to"     => (HistoricalHomologyMemberof, HasHistoricalHomologyMember, true)
+      case "not ser hom to" => (SerialHomologyMemberOf, HasSerialHomologyMember, true)
+    }
+    val structure1Text = items(1).trim
+    val structure1 = if (structure1Text.contains("^")) PostCompositionParser.parseExpression(structure1Text).get
+    else Class(IRI.create(structure1Text))
+    val taxon1 = Class(IRI.create(items(3).trim))
+    val structure2Text = items(6).trim
+    val structure2 = if (structure2Text.contains("^")) PostCompositionParser.parseExpression(structure2Text).get
+    else Class(IRI.create(structure2Text))
+    val taxon2 = Class(IRI.create(items(8).trim))
+    val ancestor = Individual(s"$uniquePrefix#ancestor")
+    var axioms = Set.empty[OWLAxiom]
+
+    val maybeEvidenceID = Option(StringUtils.stripToNull(items(12)))
+    if (!negated) {
+      axioms += (structure1 and (in_taxon some taxon1)) SubClassOf (upProperty value ancestor)
+      axioms += ancestor Type (downProperty some (structure1 and (in_taxon some taxon1)))
+      axioms += (structure2 and (in_taxon some taxon2)) SubClassOf (upProperty value ancestor)
+      axioms += ancestor Type (downProperty some (structure2 and (in_taxon some taxon2)))
+      maybeEvidenceID.foreach { evidenceID =>
+        val evidenceCode = Class(OBOUtil.iriForTermID(evidenceID))
+        val evidence = Individual(s"$uniquePrefix#evidence")
+        val pub = factory.getOWLLiteral(items(13).trim)
+        axioms += evidence Type evidenceCode
+        axioms += evidence Annotation (source, pub)
+        axioms += ancestor Fact (has_evidence, evidence)
+      }
+    }
+    axioms
+  }
+
+  val output = convertFile(input)
+  manager.saveOntology(output, IRI.create(new File(args(1))))
+
+}
