e9641353aa3e63dbe55bed7afe139758b93b6c09
Jim Balhoff <jim@balhoff.org>
2018-03-12 16:01:42 -0400

    Added file to repo.

---
+++ src/main/scala/org/phenoscape/owl/util/Conversions.scala
@@ -0,0 +1,14 @@
+package org.phenoscape.owl.util
+
+import better.files._
+import scala.io.Source
+
+object Conversions {
+
+  implicit class BetterFileOps(val self: File) extends AnyVal {
+
+    def toSource(encoding: String): Source = Source.fromFile(self.toJava, encoding)
+
+  }
+
+}
