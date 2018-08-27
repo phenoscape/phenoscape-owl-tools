cdf793476ee617b689551b22eb12cf1b8dee1298
Jim Balhoff <jim@balhoff.org>
2018-08-27 15:10:04 -0400

    Add converter for NeXML files to OWL.

---
+++ src/main/scala/org/phenoscape/owl/build/ConvertNeXML.scala
@@ -0,0 +1,23 @@
+package org.phenoscape.owl.build
+
+import java.io.File
+
+import org.phenoscape.owl.PhenexToOWL
+import org.phenoscape.owl.PropertyNormalizer
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat
+import org.semanticweb.owlapi.model.IRI
+import java.io.FileOutputStream
+
+object ConvertNeXML extends App {
+
+  val ontologyPath = args(0)
+  val nexmlFiles = args.drop(1)
+  val manager = OWLManager.createOWLOntologyManager()
+  val ontology = manager.loadOntology(IRI.create(new File(ontologyPath)))
+  nexmlFiles.foreach { file =>
+    val nexmlOnt = PropertyNormalizer.normalize(PhenexToOWL.convert(new File(file), ontology))
+    manager.saveOntology(nexmlOnt, new FunctionalSyntaxDocumentFormat(), new FileOutputStream(new File(s"$file.ofn")))
+  }
+
+}
