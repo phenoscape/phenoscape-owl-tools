92300247f37a89c8d676adb3d93eaabd1f4061bc
Shalsh <shrivastava.shal@gmail.com>
2018-09-26 14:00:43 -0400

    added commandline argument to pass output file

--- src/main/scala/org/phenoscape/owl/build/ConvertNeXML.scala
+++ src/main/scala/org/phenoscape/owl/build/ConvertNeXML.scala
@@ -12,12 +12,13 @@ import java.io.FileOutputStream
 object ConvertNeXML extends App {
 
   val ontologyPath = args(0)
-  val nexmlFiles = args.drop(1)
+  val outputFile = args(1)
+  val nexmlFiles = args.drop(2)
   val manager = OWLManager.createOWLOntologyManager()
   val ontology = manager.loadOntology(IRI.create(new File(ontologyPath)))
   nexmlFiles.foreach { file =>
     val nexmlOnt = PropertyNormalizer.normalize(PhenexToOWL.convert(new File(file), ontology))
-    manager.saveOntology(nexmlOnt, new FunctionalSyntaxDocumentFormat(), new FileOutputStream(new File(s"$file.ofn")))
+    manager.saveOntology(nexmlOnt, new FunctionalSyntaxDocumentFormat(), new FileOutputStream(new File(outputFile)))
   }
 
 }
