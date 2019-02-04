f7374ddd8db85c52d1bafaa9fbd146711807fc80
Shalsh <shrivastava.shal@gmail.com>
2019-02-04 15:37:02 -0500

    Formatting and cleaup

--- src/test/scala/org/phenoscape/owl/NegationHierarchyAsserterTest.scala
+++ src/test/scala/org/phenoscape/owl/NegationHierarchyAsserterTest.scala
@@ -8,81 +8,58 @@ import org.semanticweb.owlapi.apibinding.OWLManager
 import org.phenoscape.scowl._
 
 
-object NegationHierarchyAsserterTest extends TestSuite{
+object NegationHierarchyAsserterTest extends TestSuite {
 
-  val tests = Tests{
+  val tests = Tests {
 
-  val base = "http://owl.phenoscape.org/NegationHierarchyAsserterTest"
-  val manager = OWLManager.createOWLOntologyManager()
-  val input = getClass().getClassLoader().getResourceAsStream("NegationHierarchyAsserterTest.ofn")
-  val ontology = manager.loadOntologyFromOntologyDocument(input)
-  input.close()
+    val base = "http://owl.phenoscape.org/NegationHierarchyAsserterTest"
+    val manager = OWLManager.createOWLOntologyManager()
+    val input = getClass().getClassLoader().getResourceAsStream("NegationHierarchyAsserterTest.ofn")
+    val ontology = manager.loadOntologyFromOntologyDocument(input)
+    input.close()
 
-  'beforeAxioms - {
-    'test1 - {
-      assert(ontology.containsAxiom(Class(s"$base#B") SubClassOf Class(s"$base#A")))
+    'beforeAxioms - {
+      'test1 - {
+        assert(ontology.containsAxiom(Class(s"$base#B") SubClassOf Class(s"$base#A")))
+      }
+
+      'test2 - {
+        assert(!(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB"))))
+      }
+
+      'test3 - {
+        assert(ontology.containsAxiom(Class(s"$base#C") SubClassOf Class(s"$base#B")))
+      }
+
+      'test4 - {
+        assert(!(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC"))))
+      }
+
+      'test5 - {
+        assert(ontology.containsAxiom(Class(s"$base#B") EquivalentTo Class(s"$base#D")))
+      }
+
+      'test6 - {
+        assert(!(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD"))))
+      }
     }
 
-    'test2 - {
-      assert(!(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB"))))
-    }
+    manager.addAxioms(ontology, NegationHierarchyAsserter.assertNegationHierarchy(ontology.getAxioms().toSet))
 
-    'test3 - {
-      assert(ontology.containsAxiom(Class(s"$base#C") SubClassOf Class(s"$base#B")))
-    }
 
-    'test4 - {
-      assert(!(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC"))))
-    }
+    'afterAxioms - {
 
-    'test5 - {
-      assert(ontology.containsAxiom(Class(s"$base#B") EquivalentTo Class(s"$base#D")))
-    }
+      'test7 - {
+        assert(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB")))
+      }
 
-    'test6 - {
-      assert(!(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD"))))
+      'test8 - {
+        assert(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC")))
+      }
+
+      'test9 - {
+        assert(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD")))
+      }
     }
   }
-
-   manager.addAxioms(ontology, NegationHierarchyAsserter.assertNegationHierarchy(ontology.getAxioms().toSet))
-
-
-  'afterAxioms - {
-
-    'test7 - {
-      assert(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB")))
-    }
-
-    'test8 - {
-      assert(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC")))
-    }
-
-    'test9 - {
-      assert(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD")))
-    }
-  }
-
-
-
-
-}
-
-//  @Test
-//  def testHierarchy() {
-//    val manager = OWLManager.createOWLOntologyManager()
-//    val input = getClass().getClassLoader().getResourceAsStream("NegationHierarchyAsserterTest.ofn")
-//    val ontology = manager.loadOntologyFromOntologyDocument(input)
-//    input.close()
-//    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#B") SubClassOf Class(s"$base#A")))
-//    Assert.assertFalse(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB")))
-//    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#C") SubClassOf Class(s"$base#B")))
-//    Assert.assertFalse(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC")))
-//    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#B") EquivalentTo Class(s"$base#D")))
-//    Assert.assertFalse(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD")))
-//    manager.addAxioms(ontology, NegationHierarchyAsserter.assertNegationHierarchy(ontology.getAxioms().toSet))
-//    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#NotA") SubClassOf Class(s"$base#NotB")))
-//    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#NotB") SubClassOf Class(s"$base#NotC")))
-//    Assert.assertTrue(ontology.containsAxiom(Class(s"$base#NotB") EquivalentTo Class(s"$base#NotD")))
-//  }
-
 }
