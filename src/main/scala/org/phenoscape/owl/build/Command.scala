c62fa8a8f0aa820d37b711bb2330e6e999c4cf4a
Shalsh <shrivastava.shal@gmail.com>
2019-04-08 14:53:26 -0400

    Reformat code

--- src/main/scala/org/phenoscape/owl/build/Command.scala
+++ src/main/scala/org/phenoscape/owl/build/Command.scala
@@ -8,21 +8,20 @@ object Command extends App {
 
   val commandArgs = args.drop(1)
   args(0) match {
-    case "build-kb"             => PhenoscapeKB.main(commandArgs)
-    case "pairwise-sim"         => RunPairwiseOWLSim.main(commandArgs)
-    case "load-triples"         => DataLoader.main(commandArgs)
-    case "output-ics"           => ComputeICs.main(commandArgs)
-    //case "output-match-scores"  => //requires sparql query to DB and output tab-delimited; just run sparql query
+    case "build-kb" => PhenoscapeKB.main(commandArgs)
+    case "pairwise-sim" => RunPairwiseOWLSim.main(commandArgs)
+    case "load-triples" => DataLoader.main(commandArgs)
+    case "output-ics" => ComputeICs.main(commandArgs)
     case "output-profile-sizes" => PrintTaxonAndGeneProfileSizes.main(commandArgs)
-    //case "compute-expects"      => //run just run python directly
-    case "expects-to-triples"   => ConvertScoresToTriples.main(commandArgs)
-    case "sparql-select"        => RunSPARQLQuery.main(commandArgs)
-    case "sparql-construct"     => RunSPARQLConstruct.main(commandArgs)
-    case "sparql-update"        => RunSPARQLUpdate.main(commandArgs)
-    case "convert-nexml"        => ConvertNeXML.main(commandArgs)
+    case "expects-to-triples" => ConvertScoresToTriples.main(commandArgs)
+    case "sparql-select" => RunSPARQLQuery.main(commandArgs)
+    case "sparql-construct" => RunSPARQLConstruct.main(commandArgs)
+    case "sparql-update" => RunSPARQLUpdate.main(commandArgs)
+    case "convert-nexml" => ConvertNeXML.main(commandArgs)
     case "assert-negation-hierarchy" => NegationHierarchyAsserter.main(commandArgs)
     case "output-evolutionary-profiles" => EvolutionaryProfiles.main(commandArgs)
-    case _ => println("""
+    case _ => println(
+      """
 Valid commands are:
 
 build-kb
--- src/test/scala/org/phenoscape/owl/PostorderTest.scala
+++ src/test/scala/org/phenoscape/owl/PostorderTest.scala
@@ -27,7 +27,7 @@ object PostorderTest extends KnowledgeBaseBuilder {
   val BIGDATA_JOURNAL = new File("staging/bigdata.jnl")
   STAGING.mkdir()
   KB.mkdir()
-  
+
 
   step("Loading ontologies")
   val tbox = manager.loadOntologyFromOntologyDocument(new File(cwd + "/staging/kb/tbox.owl"))
@@ -35,39 +35,12 @@ object PostorderTest extends KnowledgeBaseBuilder {
   step("Reasoning with tbox")
   val tboxReasoner = reasoner(tbox)
 
-//  step("Loading Bigdata")
-//  val bigdataProperties = new Properties()
-//  bigdataProperties.load(new FileReader(BIGDATA_PROPERTIES))
-//  bigdataProperties.setProperty(Options.FILE, BIGDATA_JOURNAL.getAbsolutePath)
-//  val sail = new BigdataSail(bigdataProperties)
-//  val repository = new BigdataSailRepository(sail)
-//  repository.initialize()
-//  val connection = repository.getUnisolatedConnection()
-//  connection.begin()
-
-//  step("Exporting all triples to turtle file")
-//  val triplesQuery = bigdata.prepareGraphQuery(QueryLanguage.SPARQL, """
-//  CONSTRUCT {
-//   ?s ?p ?o .
-//  }
-//  FROM <http://kb.phenoscape.org/>
-//  WHERE {
-//   ?s ?p ?o .
-//  }
-//  """)
-//  val triplesOutput = new BufferedOutputStream(new FileOutputStream((KB / "kb.ttl").toJava))
-//  triplesQuery.evaluate(new TurtleWriter(triplesOutput))
-//  triplesOutput.close()
 
   step("Testing postorder part of ancestral states reconstruction")
-//  println("Triples: " + connection.getTripleStore.getStatementCount)
   val result = EvolutionaryProfiles.computePhenotypeProfiles(TaxonNode(CHORDATA), tbox, inFile)
   println("Size of profile data: " + result.size)
   result.take(100).foreach(println)
 
-//  connection.commit()
-  // close the repository connection
-//  connection.close()
 
   step("Done")
 
